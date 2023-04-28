34d6e68bf6ba057b3fdf4b206268ce5ff44b0e95
team7 <hnhyerim@gmail.com>
2023-04-28 22:39:43 +0900

    Feat: Home 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import styled from 'styled-components'
 
 function App() {
-  return <Outlet />
+  return (
+    <Wrapper>
+      <Outlet />
+    </Wrapper>
+  )
 }
 
 export default App
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 600;
+  width: 100%;
+  height: 100vh;
+`
