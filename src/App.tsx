7ab1c53eda1943c7778bcd64fe234cc5f0fa8936
team7 <hnhyerim@gmail.com>
2023-04-28 13:37:05 +0900

    Feat: Gestures part two

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import { click } from '@testing-library/user-event/dist/click'
 import { motion } from 'framer-motion'
-import React from 'react'
+import React, { useRef } from 'react'
 import styled from 'styled-components'
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null)
   return (
     <Wrapper>
-      <Box
-        drag
-        variants={BoxVariants}
-        whileHover='hover'
-        whileDrag='drag'
-        whileTap='click'
-      />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={BoxVariants}
+          whileHover='hover'
+          whileDrag='drag'
+          whileTap='click'
+        />
+      </BiggerBox>
     </Wrapper>
   )
 }
@@ -27,6 +33,17 @@ const Wrapper = styled.div`
   align-items: center;
 `
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* overflow: hidden; */
+`
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
@@ -36,7 +53,7 @@ const Box = styled(motion.div)`
 `
 
 const BoxVariants = {
-  hover: { scale: 1.5, rotateZ: 90 },
+  hover: { rotateZ: 90 },
   click: { scale: 1, borderRadius: '100px' },
   drag: { backgroundColor: 'rgb(46, 204, 113)', transition: { duration: 10 } },
 }
