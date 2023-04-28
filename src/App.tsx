6ec458a534a9c9965a3403da480aea893153af20
team7 <hnhyerim@gmail.com>
2023-04-28 20:56:35 +0900

    Feat: You Need to Watch This

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,28 @@
-import { motion, AnimatePresence } from 'framer-motion'
+import { motion } from 'framer-motion'
 import React, { useState } from 'react'
 import styled from 'styled-components'
 
 function App() {
-  const [visible, setVisible] = useState(1)
-  const [back, setBack] = useState(false)
-  const nextPlease = () => {
-    setBack(false)
-    setVisible((prev) => (prev === 10 ? 10 : prev + 1))
-  }
-  const prevPlease = () => {
-    setBack(true)
-    setVisible((prev) => (prev === 1 ? 1 : prev - 1))
-  }
+  const [clicked, setClicked] = useState(false)
+  const toggleClick = () => setClicked((prev) => !prev)
   return (
-    <Wrapper>
-      <AnimatePresence custom={back}>
-        <Box
-          custom={back}
-          variants={BoxVariants}
-          initial='entry'
-          animate='center'
-          exit='leaving'
-          key={visible}
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <button onClick={prevPlease}>prev</button>
-      <button onClick={nextPlease}>next</button>
+    <Wrapper onClick={toggleClick}>
+      <Box>
+        {!clicked ? (
+          <Circle
+            layoutId='circle'
+            style={{ borderRadius: '50px' }}
+          />
+        ) : null}
+      </Box>
+      <Box>
+        {clicked ? (
+          <Circle
+            layoutId='circle'
+            style={{ borderRadius: 0, scale: 2 }}
+          />
+        ) : null}
+      </Box>
     </Wrapper>
   )
 }
@@ -42,43 +36,24 @@ const Wrapper = styled(motion.div)`
   justify-content: center;
   align-items: center;
   background: linear-gradient(135deg, #f08, #d0e);
-  flex-direction: column;
 `
 
 const Box = styled(motion.div)`
   width: 400px;
-  height: 200px;
+  height: 400px;
   background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
-  position: absolute;
-  top: 100px;
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 28px;
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `
 
-const BoxVariants = {
-  entry: (isBack: boolean) => ({
-    x: isBack ? -500 : 500,
-    opacity: 0,
-    scale: 0,
-  }),
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 0.3,
-    },
-  },
-  leaving: (isBack: boolean) => ({
-    x: isBack ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    transition: {
-      duration: 0.3,
-    },
-  }),
-}
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  height: 100px;
+  width: 100px;
+  border-radius: 50px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`
