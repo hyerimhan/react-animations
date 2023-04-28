f4685ad0dc9f67f350f6dcdd57453f30950aa942
team7 <hnhyerim@gmail.com>
2023-04-28 12:34:53 +0900

    Feat: Variants part one

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ function App() {
   return (
     <Wrapper>
       <Box
-        transition={{ type: 'spring', delay: 0.5 }}
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, rotateZ: 360 }}
+        variants={myVars}
+        initial='start'
+        animate='end'
       />
     </Wrapper>
   )
@@ -31,3 +31,14 @@ const Box = styled(motion.div)`
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `
+
+const myVars = {
+  start: {
+    scale: 0,
+  },
+  end: {
+    scale: 1,
+    rotateZ: 360,
+    transition: { type: 'spring', delay: 0.5 },
+  },
+}
