f3c4b9dd04465d1f7b23385c3556d97b420d7530
team7 <hnhyerim@gmail.com>
2023-04-28 23:14:28 +0900

    Feat: AnimatePresence 생성

--- src/pages/AnimatePresence.tsx
+++ src/pages/AnimatePresence.tsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+import styled from 'styled-components'
 
-const AnimatePresence = () => {
-  return <div></div>
+const AniPresence = () => {
+  const [showing, setShowing] = useState(false)
+  const toggleShowing = () => setShowing((prev) => !prev)
+  return (
+    <Wrapper>
+      <button onClick={toggleShowing}>Click</button>
+      <AnimatePresence>
+        {showing ? (
+          <Box
+            variants={BoxVariants}
+            initial='initial'
+            animate='visible'
+            exit='leaving'
+          />
+        ) : null}
+      </AnimatePresence>
+    </Wrapper>
+  )
 }
 
-export default AnimatePresence
+export default AniPresence
+
+const Wrapper = styled(motion.div)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: linear-gradient(135deg, #f08, #d0e);
+`
+
+const Box = styled(motion.div)`
+  width: 400px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  position: absolute;
+  top: 100px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`
+
+const BoxVariants = {
+  initial: {
+    opacity: 0,
+    scale: 0,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    rotateZ: 360,
+  },
+  leaving: {
+    opacity: 0,
+    scale: 0,
+    y: 50,
+  },
+}
