ebb944c80e3ecc787b658abb486a365aa975cfef
team7 <hnhyerim@gmail.com>
2023-04-28 22:17:00 +0900

    Feat: 라우터 생성

---
+++ src/router.tsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter } from 'react-router-dom'
+import App from './App'
+import NotFound from './pages/NotFound'
+import Home from './pages/Home'
+import BasicAnimations from './pages/BasicAnimations'
+import Variants from './pages/Variants'
+import Gestures from './pages/Gestures'
+import MotionValues from './pages/MotionValues'
+import SVGAnimation from './pages/SVGAnimation'
+import AnimatePresence from './pages/AnimatePresence'
+import Slider from './pages/Slider'
+import Etc from './pages/Etc'
+import Modal from './pages/Modal'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        path: '/home',
+        element: <Home />,
+      },
+      { path: '/basic', element: <BasicAnimations /> },
+      { path: '/variants', element: <Variants /> },
+      { path: '/gestures', element: <Gestures /> },
+      { path: '/motionValues', element: <MotionValues /> },
+      { path: '/svgAnimation', element: <SVGAnimation /> },
+      { path: '/animationPresence', element: <AnimatePresence /> },
+      { path: '/slider', element: <Slider /> },
+      { path: '/etc', element: <Etc /> },
+      { path: '/modal', element: <Modal /> },
+    ],
+  },
+])
