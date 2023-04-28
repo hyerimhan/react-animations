83d8dd41f30a9912ee764d3902ad725ca9709a00
team7 <hnhyerim@gmail.com>
2023-04-28 23:19:57 +0900

    Feat: Etc 생성

--- src/pages/Etc.tsx
+++ src/pages/Etc.tsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { motion } from 'framer-motion'
 
 const Etc = () => {
-  return <div></div>
+  const [clicked, setClicked] = useState(false)
+  const toggleClick = () => setClicked((prev) => !prev)
+  return (
+    <Wrapper onClick={toggleClick}>
+      <Box>
+        {!clicked ? (
+          <Circle
+            layoutId='circle'
+            style={{ borderRadius: '50px' }}
+          />
+        ) : null}
+      </Box>
+      <Box>
+        {clicked ? (
+          <Circle
+            layoutId='circle'
+            style={{ borderRadius: 0, scale: 2 }}
+          />
+        ) : null}
+      </Box>
+    </Wrapper>
+  )
 }
 
 export default Etc
+
+const Wrapper = styled(motion.div)`
+  display: flex;
+`
+
+const Box = styled(motion.div)`
+  width: 400px;
+  height: 400px;
+  background-color: rgba(255, 255, 255, 1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`
+
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  height: 100px;
+  width: 100px;
+  border-radius: 50px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`
