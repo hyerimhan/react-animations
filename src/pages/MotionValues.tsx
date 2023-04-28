d38df486a0c861d2a5512b094e696e684bb7b9e9
team7 <hnhyerim@gmail.com>
2023-04-28 23:05:15 +0900

    Feat: MotionValues 생성

--- src/pages/MotionValues.tsx
+++ src/pages/MotionValues.tsx
@@ -1,7 +1,58 @@
 import React from 'react'
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  useViewportScroll,
+} from 'framer-motion'
+import styled from 'styled-components'
 
 const MotionValues = () => {
-  return <div></div>
+  const x = useMotionValue(0)
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360])
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      'linear-gradient(135deg, rgb(0, 210, 238), rgb(36, 0, 238))',
+      'linear-gradient(135deg, rgb(0, 238, 135), rgb(238, 186, 0))',
+    ]
+  )
+  const { scrollYProgress } = useViewportScroll()
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5])
+
+  return (
+    <Wrapper style={{ background: gradient }}>
+      <Box
+        style={{ x, rotateZ, scale }}
+        drag='x'
+        dragSnapToOrigin
+      />
+    </Wrapper>
+  )
 }
 
 export default MotionValues
+
+const Wrapper = styled(motion.div)`
+  height: 200vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* background: ; */
+`
+
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`
+
+const BoxVariants = {
+  hover: { rotateZ: 90 },
+  click: { scale: 1, borderRadius: '100px' },
+  drag: { backgroundColor: 'rgb(46, 204, 113)', transition: { duration: 10 } },
+}
