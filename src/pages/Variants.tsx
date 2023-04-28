03427c9ddf7bfabdc7a37743937cf94eb73518c9
team7 <hnhyerim@gmail.com>
2023-04-28 22:56:39 +0900

    Feat: Variants 생성

--- src/pages/Variants.tsx
+++ src/pages/Variants.tsx
@@ -1,7 +1,68 @@
 import React from 'react'
+import styled from 'styled-components'
+import { motion } from 'framer-motion'
 
 const Variants = () => {
-  return <div></div>
+  return (
+    <Box
+      variants={BoxVariants}
+      initial='start'
+      animate='end'
+    >
+      <Circle variants={CircleVariants} />
+      <Circle variants={CircleVariants} />
+      <Circle variants={CircleVariants} />
+      <Circle variants={CircleVariants} />
+    </Box>
+  )
 }
 
 export default Variants
+
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`
+
+const Circle = styled(motion.div)`
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  height: 70px;
+  width: 70px;
+  place-self: center;
+  border-radius: 35px;
+  background-color: white;
+`
+
+const BoxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: 'spring',
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+}
+
+const CircleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+}
